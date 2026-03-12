050850c0dfab0935cb73b449a5b39f73d9c42668
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-03-12 21:27:39 +0530

    added

--- sorting/bubblesort.java
+++ sorting/bubblesort.java
@@ -1,8 +1,27 @@
-/*compares adjacent elements and swaps it
+/* compares adjacent elements and swaps it
 after each pass the largest element goes to the end
 */
 public class bubblesort {
     public static void main(String[] args) {
-        l
+        int[] arr = {3,1,5,4,2};
+
+        search(arr);
+
+        // print sorted array
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void search(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 1; j < arr.length - i; j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
     }
 }
